9ec82723550955bcbd058b8b982ae8bc1e30d196
Humberto <hdkaratechop@gmail.com>
2023-01-18 20:34:57 -0800

    create self-join for host and attendee users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
   
-  has_many :created_events, foreign_key: "creator_id", class_name: "Post"
+  has_many :attendees, class_name: "User", foreign_key: "host_id"
+  belongs_to :host, class_name: "User", optional: true 
 
 end
