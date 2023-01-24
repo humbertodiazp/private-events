ca6a45b57d6f61dba6dc5e7a01c29af392f215fd
Humberto <hdkaratechop@gmail.com>
2023-01-23 21:23:25 -0800

    add associations for future EventAttendee model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: "User"
+    has_many :event_attendees, foreign_key: :attendee_id
+    has_many :attendees, through: :event_attendees, source: :event_attendee 
+    belongs_to :host, class_name: "User"
 end
