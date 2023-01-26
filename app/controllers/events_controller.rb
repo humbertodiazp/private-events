28a1e7c4224bebb7a0356341f947ca7b92bd26a0
Humberto <hdkaratechop@gmail.com>
2023-01-25 21:27:21 -0800

    update event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,15 @@
 class EventsController < ApplicationController
   
-  def Index
+  def index
     @events= Event.all
   end
+
+  def new
+    @events = Event.new 
+  end
+
+  def event_creator 
+    
+
 
 end
