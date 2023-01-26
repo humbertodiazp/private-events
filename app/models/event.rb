70cd2d4284d3369e86948d91030c80ada0ace072
Humberto <hdkaratechop@gmail.com>
2023-01-25 21:22:43 -0800

    update event associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
-    has_many :event_attendees, foreign_key: :attendee_id
-    has_many :attendees, through: :event_attendees, source: :event_attendee 
+    has_many :attended_events, foreign_key: :attended_event_id
+    has_many :attendees, through: :attended_events source: :event_attendee
     belongs_to :host, class_name: "User"
 end
+
+
