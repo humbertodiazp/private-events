0c3e17fb147e078d781b0ff7a3d2806a3fc7dcc4
Humberto <hdkaratechop@gmail.com>
2023-01-27 19:41:13 -0800

    fix and match association nomenclature with user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
-    has_many :event_regestries, foreign_key: :event_regestry_id
-    has_many :attendees, through: :event_regestries, source: :event_attendee
+    has_many :attended_events, foreign_key: :attended_event_id
+    has_many :attendees, through: :event_attendents, source: :event_attendee
     belongs_to :host, class_name: "User"
 end
 
 
 
-# class EventRegistering 
+# class EventAttendence 
 #     belongs_to :event_attendee, class_name: "User"
-#     belongs_to :registered_event, class_name: "Post"
+#     belongs_to :attended_event, class_name: "Post"
 #   end
