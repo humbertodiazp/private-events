8abce9edd7f86dfd3920e866981302663db83983
Humberto <hdkaratechop@gmail.com>
2023-01-29 17:12:34 -0800

    change host to creater

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,11 @@
 class Event < ApplicationRecord
     has_many :attended_events, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendents, source: :event_attendee
-    belongs_to :host, class_name: "User"
+    belongs_to :creator, class_name: "User"
 end
 
 
-
-# class EventAttendence 
+# class EventAttendents
 #     belongs_to :event_attendee, class_name: "User"
 #     belongs_to :attended_event, class_name: "Post"
 #   end
