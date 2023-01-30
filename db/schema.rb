ed3e995d4923ed73fd3c487065739bb552dd866c
Humberto <hdkaratechop@gmail.com>
2023-01-29 23:31:56 -0800

    run db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_000316) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_064946) do
+  create_table "event_attendents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "event_date"
     t.datetime "created_at", null: false
