2e45e2ec9984debbc0ca9425bccf4d9d9a3bdaf7
Humberto <hdkaratechop@gmail.com>
2023-01-29 21:02:33 -0800

    remove extra comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   has_many :attended_events, foreign_key: :event_attendee_id
   has_many :attendees, through: :event_attendents
-  has_many :created_events, class_name: "Event", foreign_key: :creator_id, 
+  has_many :created_events, class_name: "Event", foreign_key: :creator_id
 
 end
 
