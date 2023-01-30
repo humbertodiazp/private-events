fc672ab342d29a7da5ab02e5a690b2620018106c
Humberto <hdkaratechop@gmail.com>
2023-01-29 23:29:00 -0800

    add associations linking users and events

--- app/models/event_attendent.rb
+++ app/models/event_attendent.rb
@@ -1,5 +1,5 @@
 class EventAttendent < ApplicationRecord
     # class EventAttendents
-     
-
+    belongs_to :event_attendee, class_name: "User"
+    belongs_to :attended_event, class_name: "Post"
 end
